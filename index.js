6095af8ddcd4c83577dda76d248585b17dde1aea
Yurii Denkovych <yd2004@ukr.net>
2022-10-03 21:32:55 +0300

    Update .gitignore

---
+++ index.js
@@ -0,0 +1,34 @@
+const {listContacts, getContactById, removeContact, addContact} = require('./contacts')
+    
+
+async function invokeAction({ action, id, name, email, phone }) {
+   switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const lastContact = await addContact(name, email, phone);
+      console.log(lastContact);
+      break;
+
+    case "remove":
+      const deleteContact = await removeContact(id);
+      console.log(deleteContact);
+           break;
+       
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({ action: "list" });
+
+
+
+
+
